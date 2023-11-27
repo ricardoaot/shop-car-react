12e21cba67c29e1657c543796f820904f0ff6ab5
Ricardo <ricardoaot@gmail.com>
2023-11-27 17:02:52 -0500

    first version of a front page in react js

---
+++ src/components/Producto.js
@@ -0,0 +1,37 @@
+import { Component } from "react";
+import Button from "./Button";
+
+const styles = {
+    producto: {
+        border: 'solid 1px #eee',
+        boxShadow: '0 5px 5px rgb(0,0,0,0.1)',
+        with: '30%',
+        padding : '10px 15px',
+        borderRadius: '5px'
+    },
+    img:{
+        width:'100%',
+        width:200,
+        height:200
+}
+}
+class Producto extends Component {
+    render(){
+        const { producto, agregarAlCarro} = this.props
+        return (
+            <div style={styles.producto}>
+                <img style={styles.img} alt={producto.name} src={producto.img} />
+                <h3>{producto.name}</h3>
+                <p>{producto.price}</p>
+                <Button
+                    onClick = {()=> agregarAlCarro(producto)}
+                >
+                    Agregar al Carro
+                </Button> 
+                
+            </div>
+        )
+    }
+}
+
+export default Producto 
